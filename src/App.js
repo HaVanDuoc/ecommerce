857f90d5d31bf5e9791a01c3244cb1cbfbd0e7ac
Hà Văn Được <havanduoc.dev@gmail.com>
2022-12-21 16:16:37 +0700

    config router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import { publicRoutes } from "./routes";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="__havanduoc">
+      <BrowserRouter>
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Layout = route.layout || DefaultLayout;
+            const Page = route.page;
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
